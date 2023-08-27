fd811117ce84664b6b56a74fab0bc0c316e69bdd
Aleksandr Chizhikov <127817879+swag-messiah@users.noreply.github.com>
2023-08-27 23:57:43 +0300

    Add files via upload
    Ver 1.1

--- wall.go
+++ wall.go
@@ -1,21 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("Ширина %0.2f не подходит", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("Высота %0.2f не подходит", height)
+	}
+
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
 	var amount, total float64
-	amount = paintNeeded(4.2, 3.0)
+	amount, err := paintNeeded(4.2, 3.0)
+	fmt.Println(err)
+
 	fmt.Printf("%0.2f литров нужно\n", amount)
 	total += amount
-	amount = paintNeeded(5.2, 3.5)
+	amount, err = paintNeeded(5.2, 3.5)
 	fmt.Printf("%0.2f литров нужно\n", amount)
 	total += amount
 	fmt.Printf("Total: %0.2f литров нужно", total)
 }
 
-//Chech Test Demi-Monde
+//Check Test Demi-Monde
